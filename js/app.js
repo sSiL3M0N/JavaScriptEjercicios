a58e20121c3a53e727516a6dd7a6f4fd76fcb9cd
sS¡L3M0N <24_de_7@protonmail.com>
2022-06-17 14:33:28 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.title="AYuda"
+
+console.log(document)
+console.log(document.URL)
+console.log(document.images)
+console.log(document.all)
+console.log(document.forms)
+console.log(document.all[6])
+console.log(document.images)
+console.log(document.all[18].textContent)
+
+
+
+
