35bb908a1c741d6b5558b769ac46e434f89e12e3
sSiL3M0N <24_de_7@protonmail.com>
2022-07-29 19:08:03 -0500

    Nuevas confifuración EMAC6

---
+++ js/ema6.js
@@ -0,0 +1,7 @@
+let nombre="Erlin";
+let apellido="Sangay";
+let direccion="Avenida"
+
+console.log(nombre.includes("lin"));
+console.log(apellido.startsWith('San'));
+console.log(direccion.endsWith('eda'));
